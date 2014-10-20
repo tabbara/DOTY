1d1d3082718cf44da56d50d8e71216bdc54ac09a
tabbara <abbara@gmail.com>
2014-10-20 15:33:28 +0200

    new CSS style for cards

--- www/js/main.js
+++ www/js/main.js
@@ -16,11 +16,15 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-
     },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
 
+    setDayColors: function () {
+        var colors = ["#ea493b", "#7fccbd", "#81c256", "#f6d24c",
+                      "#9765b8", "#73a6db", "#73a6db", "#e6b294"];
+
+        $(".day-title").each( function(i) {
+            console.log("b");
+            this.style.background = colors[i % 8];
+        });
     }
 };
