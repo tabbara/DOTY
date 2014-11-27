fe42f6bab1e73d64900f201ac15694e0a9936ebe
tabbara <abbara@gmail.com>
2014-11-27 15:24:44 +0100

    pre-refactor

--- www/js/main.js
+++ www/js/main.js
@@ -1,260 +1,343 @@
-app.controller('DaysCtrl', function($scope, $http, $stateParams, $location, $ionicPopup, $timeout, fbAPI) {
+app.factory('signinFac', function($rootScope) {
+  var fac = {};
 
-    $scope.profile = {
-        "logged": false
+  var signedIn = false; //
+
+  fac.checkSignin = function() {
+    $rootScope.$broadcast('signedIn', { 'signedIn' : signedIn });
+    return signedIn;
+  }
+
+  fac.signin = function() {
+    signedIn = true;
+    $rootScope.$broadcast('signedIn', { 'signedIn' : signedIn });
+  }
+
+  return fac;
+});
+
+app.controller('signinController', function(signinFac) {
+
+  this.signinData = {};
+
+  this.signin = function() {
+    console.log('log in attempt');
+  };
+});
+
+app.controller('DaysCtrl', function($scope, $http, $stateParams, $location, $ionicPopup, $timeout, fbAPI, $ionicModal, signinFac) {
+
+  $scope.profile = {
+    "logged": false
+  }
+
+  $scope.views = [
+    { title: 'Home', url: "/", icon: "ion-home" },
+    { title: 'Calendar', url: "calendar", icon: "ion-ios7-calendar-outline" },
+    { title: 'Explore', url: "explore", icon: "ion-earth" },
+    { title: 'Favorites', url: "favorites", icon: "ion-ios7-heart" },
+    { title: 'Profile', url: "profile", icon: "ion-person" },
+    { title: 'Settings', url: "settings", icon: "ion-ios7-gear" }
+  ];
+
+  $scope.setFavorite = function (dayid) {
+    var lookup = {};
+    for (var i = 0, len = $scope.days.length; i < len; i++) {
+      lookup[$scope.days[i].id] = i;
     }
+    $scope.days[lookup[dayid]].liked = !$scope.days[lookup[dayid]].liked;
 
-    $scope.views = [
-        { title: 'Home', url: "/", icon: "ion-home" },
-        { title: 'Calendar', url: "calendar", icon: "ion-ios7-calendar-outline" },
-        { title: 'Explore', url: "explore", icon: "ion-earth" },
-        { title: 'Favorites', url: "favorites", icon: "ion-ios7-heart" },
-        { title: 'Profile', url: "profile", icon: "ion-person" },
-        { title: 'Settings', url: "settings", icon: "ion-ios7-gear" }
-    ];
+    if($scope.days[lookup[dayid]].liked){
+      var alertPopup = $ionicPopup.alert({
+        title: $scope.days[lookup[dayid]].title,
+        template: 'has been added to your favourites!',
+        okText: "close"
+      });
 
-    $scope.setFavorite = function (dayid) {
-        var lookup = {};
-        for (var i = 0, len = $scope.days.length; i < len; i++) {
-            lookup[$scope.days[i].id] = i;
-        }
-        $scope.days[lookup[dayid]].liked = !$scope.days[lookup[dayid]].liked;
+      $timeout(function() {
+        alertPopup.close();
+      }, 2750);
+    }
+  };
 
-        if($scope.days[lookup[dayid]].liked){
-            var alertPopup = $ionicPopup.alert({
-                 title: $scope.days[lookup[dayid]].title,
-                 template: 'has been added to your favourites!',
-                 okText: "close"
-            });
+  $scope.$on('signedIn', function(event,message) {
+    if(message.signedIn === true) {
+      console.log('LOGGED IN!');
+      $scope.signinModal.hide();
+    } else {
+      console.log('NOT LOGGED IN!');
+      $scope.signinModal.show();
+    }
+  });
 
-            $timeout(function() {
-                alertPopup.close();
-            }, 2750);
-        }
-    };
+  $scope.signin = function(){
+    signinFac.signin();
+  }
+
+  $timeout(function() {
+    signinFac.checkSignin();
+  }, 500);
 
 
-//        });
+  $ionicModal.fromTemplateUrl('modals/signinModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: false,
+    hardwareBackButtonClose: false
+  }).then(function(modal) {
+    $scope.signinModal = modal;
+    console.log('Creating Signin Modal');
+  });
+  $scope.signinModalOpen = function() {
+    $scope.signinModal.show();
+    console.log('Opening In Modal');
+  };
+  $scope.signinModalClose = function() {
+    $scope.signinModal.hide();
+    console.log('Closing In Modal');
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.signinModal.remove();
+    console.log('Destroying In Modal');
+  });
 
-//    $http.get("http://app.daysoftheyear.com/api.php?date_start=16-10-2014&date_end=18-10-2014&limit=100")
-//        .then(function(res){
-//    $scope.days = res.data.days;
-//
-//    $.each($scope.days, function (index, _dayObj) {
-//        _dayObj.liked = false;
-//        _dayObj.title = _dayObj.title.replace("&#8217;","'");
-//
-//        _dayObj.content = _dayObj.content.replace(/\r\n/g,"<BR>").replace(/<a>/g, "").replace(/<\/?a[^>]*>/g, "");
-//
-//    });
+  $ionicModal.fromTemplateUrl('modals/signupModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: false,
+    hardwareBackButtonClose: false
+  }).then(function(modal) {
+    $scope.signupModal = modal;
+    console.log('Creating Signup Modal');
+  });
+  $scope.signupModalOpen = function() {
+    $scope.signupModal.show();
+    console.log('Opening Up Modal');
+  };
+  $scope.signupModalClose = function() {
+    $scope.signupModal.hide();
+    console.log('Closing Up Modal');
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.signupModal.remove();
+    console.log('Destroying Up Modal');
+  });
 
-//////// Set 'dayObj' to the selected day in the JSON-loaded list of days
-//      Old Style (faster, but doesn't work on reload). <a on-tap="setPage({{day}})">
-//        $scope.setPage = function (day) {
-//        $scope.dayObj = day;
-//    }
-
-//    if ($location.path() != "/") {
-//        $scope.dayID = [{id: $stateParams.dayID}];
-//        var lookup = {};
-//        for (var i = 0, len = $scope.days.length; i < len; i++) {
-//            lookup[$scope.days[i].id] = $scope.days[i];
-//        }
-//        $scope.dayObj = lookup[$stateParams.dayID.replace(/:/g,"")];
-//    }
-//
-//    });
-////////
+  $ionicModal.fromTemplateUrl('modals/signupExtraModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: true,
+    hardwareBackButtonClose: true
+  }).then(function(modal) {
+    $scope.signupExtraModal = modal;
+    console.log('Creating SignupExtra Modal');
+  });
+  $scope.signupExtraModalOpen = function() {
+    $scope.signupExtraModal.show();
+    console.log('Opening UpExtra Modal');
+  };
+  $scope.signupExtraModalClose = function() {
+    $scope.signupExtraModal.hide();
+    console.log('Closing UpExtra Modal');
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.signupExtraModal.remove();
+    console.log('Destroying UpExtra Modal');
+  });
 
 });
 
+
 app.controller('profileCtrl', function($scope, fbAPI) {
-    var showLoginButton = false;
+  var showLoginButton = false;
 
-    $scope.loginButton = function() {
-        showLoginButton = true;
-    }
+  $scope.loginButton = function() {
+    showLoginButton = true;
+  }
 
-    $scope.logoutButton = function() {
-        fbAPI.logout(function() {
-            showLoginButton = false;
-            $scope.fbName = "";
-            $scope.fbImage = "";
-        });
-    }
+  $scope.logoutButton = function() {
+    fbAPI.logout(function() {
+      showLoginButton = false;
+      $scope.fbName = "";
+      $scope.fbImage = "";
+    });
+  }
 
-    $scope.loginButtonShow = function() {
-        return showLoginButton;
-    }
+  $scope.loginButtonShow = function() {
+    return showLoginButton;
+  }
 
-    $scope.facebookLogin = function() {
-        fbAPI.login(function (){
-            $scope.facebookGetInfo();
-            $scope.loginButton();
-        });
-    };
+  $scope.facebookLogin = function() {
+    fbAPI.login(function (){
+      $scope.facebookGetInfo();
+      $scope.loginButton();
+    });
+  };
 
-    $scope.facebookGetInfo = function() {
-        var request = fbAPI.getInfo(),
+  $scope.facebookGetInfo = function() {
+    var request = fbAPI.getInfo(),
         response;
 
-        request.onreadystatechange = function () {
+    request.onreadystatechange = function () {
 
-            if (request.readyState === 4) {
-                if (request.status === 200) {
-                    response = JSON.parse(request.responseText);
-                    $scope.fbName = response.name;
-                    $scope.fbImage = "http://graph.facebook.com/" +
-                        response.id + "/picture?type=large";
-                    $scope.$apply();
-                } else {
-                    console.log("error with login request");
-                }
-            }
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          response = JSON.parse(request.responseText);
+          $scope.fbName = response.name;
+          $scope.fbImage = "http://graph.facebook.com/" +
+            response.id + "/picture?type=large";
+          $scope.$apply();
+        } else {
+          console.log("error with login request");
         }
-    };
+      }
+    }
+  };
 
-//    $scope.facebookGetInfo();
+  //    $scope.facebookGetInfo();
 
 });
 
 app.factory('queryAPI', function($http) {
-    var fac = {};
+  var fac = {};
 
-    fac.getDayToday = function() {
+  fac.getDayToday = function() {
 
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1;
-        var yyyy = today.getFullYear();
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
 
-        if(dd<10) { dd='0'+dd; }
-        if(mm<10) { mm='0'+mm; }
+    if(dd<10) { dd='0'+dd; }
+    if(mm<10) { mm='0'+mm; }
 
-        today = dd+'-'+mm+'-'+yyyy;
+    today = dd+'-'+mm+'-'+yyyy;
 
-        return $http.get("https://www.daysoftheyear.com/app/days/?date_start="
-        + today +"&date_end=" + today + "&limit=100")
-        .then(function(res) {
-            return res.data;
-        });
-    }
+    return $http.get("https://www.daysoftheyear.com/app/days/?date_start="
+                     + today +"&date_end=" + today + "&limit=100")
+    .then(function(res) {
+      return res.data;
+    });
+  }
 
-    fac.getDayById = function(id) {
+  fac.getDayById = function(id) {
 
-        return $http.get("https://www.daysoftheyear.com/app/days/?day_ids="
-        + id)
-        .then(function(res) {
-            return res.data;
-        });
-    }
+    return $http.get("https://www.daysoftheyear.com/app/days/?day_ids="
+                     + id)
+    .then(function(res) {
+      return res.data;
+    });
+  }
 
-    // Clean Title
-    fac.cleanDayTitle = function(daysArray) {
-        $.each(daysArray, function (index, _dayObj) {
-            _dayObj.title = _dayObj.title.replace("&#8217;","'");
-        });
-        return daysArray;
-    }
+  // Clean Title
+  fac.cleanDayTitle = function(daysArray) {
+    $.each(daysArray, function (index, _dayObj) {
+      _dayObj.title = _dayObj.title.replace("&#8217;","'");
+    });
+    return daysArray;
+  }
 
-    // Clean Title and Content
-    fac.cleanDay = function(daysArray) {
-        $.each(daysArray, function (index, _dayObj) {
-            _dayObj.title = _dayObj.title.replace("&#8217;","'");
-            _dayObj.content = _dayObj.content
-            .replace(/\r\n/g,"<BR>")
-            .replace(/<a>/g, "").replace(/<\/?a[^>]*>/g, "");
-        });
-        return daysArray;
-    };
+  // Clean Title and Content
+  fac.cleanDay = function(daysArray) {
+    $.each(daysArray, function (index, _dayObj) {
+      _dayObj.title = _dayObj.title.replace("&#8217;","'");
+      _dayObj.content = _dayObj.content
+      .replace(/\r\n/g,"<BR>")
+      .replace(/<a>/g, "").replace(/<\/?a[^>]*>/g, "");
+    });
+    return daysArray;
+  };
 
-    return fac;
+  return fac;
 });
 
 app.controller('homeCtrl', function($scope, queryAPI) {
-    queryAPI.getDayToday().then(function(data) {
-        $scope.days = queryAPI.cleanDay(data.days);
-    });
+  queryAPI.getDayToday().then(function(data) {
+    $scope.days = queryAPI.cleanDay(data.days);
+  });
 });
 
 app.controller('daypageCtrl', function($scope, queryAPI, $stateParams) {
 
-    var pageID = $stateParams.dayID.replace(/:/g,"");
+  var pageID = $stateParams.dayID.replace(/:/g,"");
 
-    queryAPI.getDayById(pageID).then(function(data) {
-        $scope.dayObj = queryAPI.cleanDay(data.days)[0];
-    });
+  queryAPI.getDayById(pageID).then(function(data) {
+    $scope.dayObj = queryAPI.cleanDay(data.days)[0];
+  });
 });
 
 app.config( function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
-    $stateProvider
-    .state('/', {
-      url: "/",
-      templateUrl: "views/home.html",
-      controller: "homeCtrl"
-    })
-    .state('calendar', {
-      url: "/calendar",
-      templateUrl: "views/calendar.html"
-    })
-    .state('explore', {
-      url: "/explore",
-      templateUrl: "views/explore.html"
-    })
-    .state('favorites', {
-      url: "/favorites",
-      templateUrl: "views/favorites.html",
-    })
-    .state('profile', {
-      url: "/profile",
-      templateUrl: "views/profile.html",
-      controller: "profileCtrl"
-    })
-    .state('settings', {
-      url: "/settings",
-      templateUrl: "views/settings.html"
-    })
-    .state('daypage', {
-      url: "/:dayID",
-      templateUrl: "views/daypage.html",
-      controller: "daypageCtrl"
-    })
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+  .state('/', {
+    url: "/",
+    templateUrl: "views/home.html",
+    controller: "homeCtrl"
+  })
+  .state('calendar', {
+    url: "/calendar",
+    templateUrl: "views/calendar.html"
+  })
+  .state('explore', {
+    url: "/explore",
+    templateUrl: "views/explore.html"
+  })
+  .state('favorites', {
+    url: "/favorites",
+    templateUrl: "views/favorites.html",
+  })
+  .state('profile', {
+    url: "/profile",
+    templateUrl: "views/profile.html",
+    controller: "profileCtrl"
+  })
+  .state('settings', {
+    url: "/settings",
+    templateUrl: "views/settings.html"
+  })
+  .state('daypage', {
+    url: "/:dayID",
+    templateUrl: "views/daypage.html",
+    controller: "daypageCtrl"
+  })
 });
 
 var jsapp = {
-    // Application Constructor
+  // Application Constructor
 
-    initialize: function () {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-    },
+  initialize: function () {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function () {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function () {
+  },
 
-    setDayColors: function () {
-        var colors = ["#ea493b", "#7fccbd", "#81c256", "#f6d24c",
-                      "#9765b8", "#73a6db", "#73a6db", "#e6b294"];
+  setDayColors: function () {
+    var colors = ["#ea493b", "#7fccbd", "#81c256", "#f6d24c",
+                  "#9765b8", "#73a6db", "#73a6db", "#e6b294"];
 
-        $(".day-title").each( function(i) {
-            this.style.background = colors[i % 8];
-        });
+    $(".day-title").each( function(i) {
+      this.style.background = colors[i % 8];
+    });
 
-        $(".day-image-container").each( function(i) {
-            this.style["border-color"] = colors[i % 8];
-        });
+    $(".day-image-container").each( function(i) {
+      this.style["border-color"] = colors[i % 8];
+    });
 
-        $(window).trigger('resize');
+    $(window).trigger('resize');
 
-    }
+  }
 };
