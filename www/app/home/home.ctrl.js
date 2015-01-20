1f576d6de9a80ca9802d1e3edaf3203b0138f986
tabbara <abbara@gmail.com>
2015-01-20 15:10:57 +0100

    Added previous and next buttons to home overview, changed styling of all buttons

--- www/app/home/home.ctrl.js
+++ www/app/home/home.ctrl.js
@@ -1,7 +1,19 @@
 angular.module('homeModule')
-.controller('homeCtrl', function ($scope, queryAPI, $ionicSideMenuDelegate) {
+.controller('homeCtrl', function ($scope, queryAPI, $ionicSideMenuDelegate, $stateParams) {
 
   $ionicSideMenuDelegate.canDragContent(true);
+
+  var tempDate = new Date();
+  var tempToday = Math.round(Date.UTC(tempDate.getFullYear(), tempDate.getMonth(), tempDate.getDate()) / 1000);
+
+  $scope.page = {};
+  $scope.page.timestamp = tempToday;
+  $scope.page.date = new Date(Math.round($scope.page.timestamp * 1000));
+  $scope.page.previous = { timestamp: $scope.page.timestamp - 86400 };
+  $scope.page.next = { timestamp: $scope.page.timestamp + 86400 }
+  $scope.page.dateName = "Today";
+
+  var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
   queryAPI.getDayToday()
   .then(function(data) {
@@ -18,3 +30,16 @@ angular.module('homeModule')
     console.log(status);
   });
 });
+
+//$scope.page.dateName = $scope.page.date.getDate() + " " + monthNames[$scope.page.date.getMonth()] + " " + $scope.page.date.getFullYear();
+//console.log('page date is: ' + $scope.page.dateName);
+//
+//if (dateToday.valueOf() === $scope.page.timestamp) { $scope.page.dateName = "Today"; }
+//if (dateToday.valueOf() === $scope.page.timestamp + 86400) { $scope.page.dateName = "Yesterday" }
+//if (dateToday.valueOf() === $scope.page.timestamp - 86400) { $scope.page.dateName = "Tomorrow" }
+//
+//console.log(dateToday);
+//console.log(dateToday.valueOf()+86400);
+//console.log(dateToday.valueOf()-86400);
+//
+//console.log($scope.page);
