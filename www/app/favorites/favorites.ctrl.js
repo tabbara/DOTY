7ca412e8267c813b25d434af2b0879a3e7352413
tabbara <abbara@gmail.com>
2015-02-08 15:51:11 +0100

    Added loading indication to favorites page and some tips when the user has no favorites

--- www/app/favorites/favorites.ctrl.js
+++ www/app/favorites/favorites.ctrl.js
@@ -1,22 +1,39 @@
 angular.module('favoritesModule')
-.controller('favoritesCtrl', function ($scope, queryAPI, $rootScope) {
+.controller('favoritesCtrl', function ($scope, queryAPI, $rootScope, $ionicLoading) {
 
   if ($rootScope.userData.pc_days.length) {
-
+    $scope.pageLoading = {
+      status: true,
+      loading: $ionicLoading.show({
+        template: '<div class="spinner-animation"></div>',
+        noBackdrop: false
+      })
+    }
     queryAPI.getDayById($rootScope.userData.pc_days)
     .then(function(data) {
       if (data.status.code === 100) {
+        $scope.pageLoading.status = false;
+        $ionicLoading.hide();
+
         queryAPI.cleanDay(data.result)
         .then(function (daysObject) {
           $scope.days = daysObject;
-//          queryAPI.setDayColors();
+          //          queryAPI.setDayColors();
         });
       } else {
+        $scope.pageLoading.status = false;
+        $ionicLoading.hide();
         console.log('Error retrieving DaysById: ' +data.status.code);
       }
     }, function (status) {
+      $scope.pageLoading.status = false;
+      $ionicLoading.hide();
       console.log(status);
     });
+  } else {
+    $scope.pageLoading = {
+      status: false
+    }
   }
 
 });
